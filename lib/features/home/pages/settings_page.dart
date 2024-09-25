e7bbf8d74e552b03fd584352fd9a151d0c6a40c0
kent0505 <otabekyusupov0550@gmail.com>
2024-09-26 04:51:03 +0500

    settings page

--- lib/features/home/pages/settings_page.dart
+++ lib/features/home/pages/settings_page.dart
@@ -1,7 +1,10 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
+import '../../../core/config/app_colors.dart';
 import '../../../core/utils.dart';
 import '../../../core/widgets/page_title.dart';
+import '../../../core/widgets/texts/text_r.dart';
 
 class SettingsPage extends StatefulWidget {
   const SettingsPage({super.key});
@@ -18,7 +21,69 @@ class _SettingsPageState extends State<SettingsPage> {
         SizedBox(height: 46 + getStatusBar(context)),
         const PageTitle('Settings'),
         const SizedBox(height: 43),
+        const _Tile(
+          id: 1,
+          title: 'Terms of use',
+        ),
+        const SizedBox(height: 16),
+        const _Tile(
+          id: 2,
+          title: 'Privacy Policy',
+        ),
+        const SizedBox(height: 16),
+        const _Tile(
+          id: 3,
+          title: 'Support page',
+        ),
+        const SizedBox(height: 16),
+        const _Tile(
+          id: 4,
+          title: 'Share with friends',
+        ),
+        const SizedBox(height: 16),
+        const _Tile(
+          id: 5,
+          title: 'Subscription info',
+        ),
       ],
+    );
+  }
+}
+
+class _Tile extends StatelessWidget {
+  const _Tile({
+    required this.id,
+    required this.title,
+  });
+
+  final int id;
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 45,
+      margin: const EdgeInsets.symmetric(horizontal: 32),
+      decoration: BoxDecoration(
+        color: AppColors.navbar,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: CupertinoButton(
+        onPressed: () {},
+        padding: EdgeInsets.zero,
+        child: Row(
+          children: [
+            const SizedBox(width: 18),
+            SvgPicture.asset('assets/settings/settings$id.svg'),
+            const SizedBox(width: 18),
+            Expanded(
+              child: TextM(title, fontSize: 16),
+            ),
+            SvgPicture.asset('assets/settings/arrow-right.svg'),
+            const SizedBox(width: 25),
+          ],
+        ),
+      ),
     );
   }
 }
